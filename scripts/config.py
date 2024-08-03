90a71692b0cefc59c382aa4ecba7fb4e87830061
Awal Mula <webmaster@realco.id>
2024-08-03 14:50:10 +0700

    delete scripts

--- scripts/config.py
+++
@@ -1,3 +0,0 @@
-user_data_dir = "/home/rajab/snap/chromium/common/chromium"
-profile_dir = "Profile 2"
-download_directory = '/home/rajab/laravel/myproject/realco_dashboard/scripts/downloaded_excell'
