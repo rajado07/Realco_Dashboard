9982c68706f8510cf5f44414c74e515fda76f265
Awal Mula <webmaster@realco.id>
2024-10-04 16:53:24 +0700

    [update] data checker for TiktokLiveStreamingData and TiktokVideoAnalyticsData

--- app/Http/Controllers/DataCheckerController.php
+++ app/Http/Controllers/DataCheckerController.php
@@ -7,16 +7,15 @@ use App\Models\ShopeeBrandPortalAdsData;
 use App\Models\ShopeeSellerCenterCoinData;
 use App\Models\ShopeeSellerCenterLiveStreamingData;
 use App\Models\ShopeeSellerCenterVoucherData;
-
 use App\Models\TiktokPsaData;
 use App\Models\TiktokLsaData;
 use App\Models\TiktokVsaData;
 
 use App\Models\TiktokGmvData;
 use App\Models\TiktokLiveStreamingData;
+use App\Models\TiktokVideoAnalyticsData;
 use App\Models\TiktokProductAnalyticsData;
 use App\Models\TiktokPromotionAnalyticsData;
-use App\Models\TiktokVideoAnalyticsData;
 
 use App\Models\MetaCpasData;
 use App\Models\Brand;
@@ -34,19 +33,36 @@ class DataCheckerController extends Controller
         $endDate = Carbon::parse($request->input('end_date', Carbon::now()->toDateString()))->toDateString(); // Default: hari ini
         $startDate = Carbon::parse($request->input('start_date', Carbon::now()->subMonth()->toDateString()))->toDateString(); // Default: sebulan ke belakang
 
-        // Log::info('Start Date: ' . $startDate);
-        // Log::info('End Date: ' . $endDate);
-
         $models = [
             'shopee_brand_portal_shop' => ShopeeBrandPortalShopData::class,
             'shopee_brand_portal_ads' => ShopeeBrandPortalAdsData::class,
             'shopee_seller_center_coins' => ShopeeSellerCenterCoinData::class,
             'shopee_seller_center_live_streaming' => ShopeeSellerCenterLiveStreamingData::class,
             'shopee_seller_center_voucher' => ShopeeSellerCenterVoucherData::class,
+
             'meta_cpas' => MetaCpasData::class,
+
             'tiktok_psa' => TiktokPsaData::class,
             'tiktok_lsa' => TiktokLsaData::class,
             'tiktok_vsa' => TiktokVsaData::class,
+
+            'tiktok_gmv' => TiktokGmvData::class,
+            'tiktok_live_streaming_own' => [
+                'model' => TiktokLiveStreamingData::class,
+                'type' => 'own',
+            ],
+            'tiktok_live_streaming_affiliate' => [
+                'model' => TiktokLiveStreamingData::class,
+                'type' => 'affiliate',
+            ],
+            'tiktok_video_analytics_own' => [
+                'model' => TiktokVideoAnalyticsData::class,
+                'type' => 'own',
+            ],
+            'tiktok_video_analytics_affiliate' => [
+                'model' => TiktokVideoAnalyticsData::class,
+                'type' => 'affiliate',
+            ],
         ];
 
         $missingDatesData = [];
@@ -55,7 +71,16 @@ class DataCheckerController extends Controller
         $brands = Brand::all();
         $marketPlaces = MarketPlace::all();
 
-        foreach ($models as $type => $model) {
+        foreach ($models as $type => $modelData) {
+            // Cek apakah $modelData adalah array (untuk tipe dengan filter type)
+            if (is_array($modelData)) {
+                $model = $modelData['model'];
+                $typeValue = $modelData['type'];
+            } else {
+                $model = $modelData;
+                $typeValue = null;
+            }
+
             $tableName = (new $model)->getTable();
             $hasMarketPlaceId = Schema::hasColumn($tableName, 'market_place_id');
 
@@ -63,10 +88,16 @@ class DataCheckerController extends Controller
                 if ($hasMarketPlaceId) {
                     foreach ($marketPlaces as $marketPlace) {
                         // Ambil tanggal dari database untuk setiap brand dan market_place_id
-                        $existingDates = $model::where('brand_id', $brand->id)
+                        $existingDatesQuery = $model::where('brand_id', $brand->id)
                             ->where('market_place_id', $marketPlace->id)
-                            ->whereBetween('data_date', [$startDate, $endDate])
-                            ->distinct()
+                            ->whereBetween('data_date', [$startDate, $endDate]);
+
+                        // Tambahkan filter type jika ada
+                        if ($typeValue) {
+                            $existingDatesQuery->where('type', $typeValue);
+                        }
+
+                        $existingDates = $existingDatesQuery->distinct()
                             ->pluck('data_date')
                             ->map(function ($date) {
                                 return Carbon::parse($date)->toDateString();
@@ -94,9 +125,15 @@ class DataCheckerController extends Controller
                     }
                 } else {
                     // Cek tanpa market_place_id
-                    $existingDates = $model::where('brand_id', $brand->id)
-                        ->whereBetween('data_date', [$startDate, $endDate])
-                        ->distinct()
+                    $existingDatesQuery = $model::where('brand_id', $brand->id)
+                        ->whereBetween('data_date', [$startDate, $endDate]);
+
+                    // Tambahkan filter type jika ada
+                    if ($typeValue) {
+                        $existingDatesQuery->where('type', $typeValue);
+                    }
+
+                    $existingDates = $existingDatesQuery->distinct()
                         ->pluck('data_date')
                         ->map(function ($date) {
                             return Carbon::parse($date)->toDateString();
